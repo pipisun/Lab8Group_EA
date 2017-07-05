e88ec960ace55b8a90994ad2e1db7b4c62a6104d
Jeremy <sst1978@163.com>
2017-07-05 12:59:04 -0500

    init

---
+++ src/main/java/edu/mum/service/impl/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package edu.mum.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.mum.dao.GenericDao;
+import edu.mum.dao.UserDao;
+import edu.mum.domain.User;
+import edu.mum.validation.ServiceValidation;
+
+@Service
+@Transactional 
+public class UserServiceImpl implements edu.mum.service.UserService {
+	
+ 	@Autowired
+	private UserDao userDao;
+
+     public void save( User user) {  		
+  		userDao.save(user);
+ 	}
+  	
+  	
+ 	public List<User> findAll() {
+		return (List<User>)userDao.findAll();
+	}
+
+	public User findByEmail(String email) {
+		return userDao.findByEmail(email);
+	}
+	
+  	public User update(User user) {
+		 return userDao.update(user);
+
+	}
+
+	
+ 	public User testRefresh(User user) {
+		user.setEmail("Lotta@Doe.com");
+		  userDao.save(user);
+		
+		  return user;
+	}
+
+
+	@Override
+ 	public User findOne(Long id) {
+		 
+		return userDao.findOne(id);
+	}
+ 
+
+}
